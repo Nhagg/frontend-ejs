9e47da1ff8a94e4de50f8877cf22fc03739405a4
nhalh <nhalh169@gmail.com>
2020-07-18 16:21:13 +0700

    add footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -2,23 +2,45 @@
   <footer class="ejs-footer">
     <div class="container">
       <div class="row">
-        <div class="col-sm-9">
+        <div class="col-sm-4">
+          <h4>ĐỊA CHỈ LIÊN HỆ</h4>
           <div>
-            Công ty cổ phần tư vấn kết nối Việt Nhật
-            <br />
-            JVCONNECT CONSULTING J.S.C
-            <br />
-            Địa chỉ: Tầng 5 - Tòa nhà Ecolife - 58 Tố Hữu - Trung Văn - Nam Từ
-            Liêm - Hà Nội
-            <br />
-            SĐT: <a href="tel:0988848484">0988848484</a>
+            Công ty cổ phần tư vấn kết nối Việt Nhật JVCONNECT CONSULTING J.S.C
+          </div>
+          <div class="mt-2">
+            Tầng 5 - Tòa nhà Ecolife - 58 Tố Hữu - Trung Văn - Nam Từ Liêm - Hà
+            Nội
+          </div>
+          <div class="mt-2">
+            Room 3210 - A1 Tower, Ecolife Capital Building, 58 Tố Hữu, Nam Từ
+            Liêm Dist, Hanoi city, Vietnam.
+          </div>
+          <h5 class="mt-4">
+            ©2020 Allrights reserved JVConnect JSC
+          </h5>
+        </div>
+        <div class="col-sm-4">
+          <h4>Hotline</h4>
+          <div>
+            Trung tâm đào tạo:
+            <a href="tel:+84 24 666 666 84">(+84) 24 666 666 84</a>
+          </div>
+          <div class="mt-2">
+            Hỗ trợ học viên:
+            <a href="tel:+84 24 666 666 84">(+84) 24 666 666 84</a>
           </div>
         </div>
-        <div class="col-sm-3">
-          <br />
-          <br />
-          <br />
-          Copyright © 2020 All Rights Reserved
+        <div class="col-sm-4">
+          <h4>KHÓA HỌC TẠI JVCONNECT</h4>
+          <div>
+            Học định hướng
+            <br class="mt-2" />
+            Học N5
+            <br class="mt-2" />
+            Học N4
+            <br class="mt-2" />
+            Học chuyên nghành <br />Học văn hóa Nhật bản
+          </div>
         </div>
       </div>
     </div>
