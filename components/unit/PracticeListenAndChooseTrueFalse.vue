00edc4663c870c82618cbb604d3ee2585fe577a3
Nguyen Ngoc Chien <chienkakashi@gmail.com>
2021-01-05 15:56:21 +0700

    small check

--- components/unit/PracticeListenAndChooseTrueFalse.vue
+++ components/unit/PracticeListenAndChooseTrueFalse.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col text-center">
         <div class="speak-title">
-          <h2 class="mr-3">Nghe và chọn đúng sai</h2>
+          <h2 class="mr-3">Nghe và chọn đúng sai test</h2>
           <div class="volume-icon" @click="playAudio">
             <i class="fa fa-volume-up"></i>
           </div>
