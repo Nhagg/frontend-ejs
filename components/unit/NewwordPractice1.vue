0122a9b22b5d401f4916c99ae47de44a53ac0241
Nguyen Ngoc Chien <chienkakashi@gmail.com>
2021-01-08 15:17:10 +0700

    fix bug

--- components/unit/NewwordPractice1.vue
+++ components/unit/NewwordPractice1.vue
@@ -14,7 +14,7 @@
         <div v-for="i in listAnswer" :key="i" class="col-sm-6">
           <div class="item-answer" @click="() => checkAnswer(i)">
             <div class="item-img ratio-4-3">
-              <img :src="$getItemImg(unit, item, i)" alt="" />
+              <img :src="getImg(i)" alt="" />
               <i
                 v-if="userAnswer !== null && i === 1"
                 class="fas fa-check-circle text-success"
